a3db5cfb71bf3a0f413628e17d368002da8b0622
tomcoso <tomasdessy0@gmail.com>
2022-11-07 11:19:51 +0000

    setup firebase / firestore

---
+++ src/firestore.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqFpWgrCabpwnkNSuyvzu4Vis-7iszIRw",
+  authDomain: "place-tag-3eed3.firebaseapp.com",
+  projectId: "place-tag-3eed3",
+  storageBucket: "place-tag-3eed3.appspot.com",
+  messagingSenderId: "450958196420",
+  appId: "1:450958196420:web:b4dd04cf1ad17ccbc11391",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
